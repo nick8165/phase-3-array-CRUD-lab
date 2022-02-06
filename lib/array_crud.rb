2dfd77e11e640e3940701168336a45fa1f7f0b85
nicholas shawn freeman <nick_freeman65@yahoo.com>
2022-02-06 11:31:32 -0600

    c

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,41 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  new_array = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
+  array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
+  array 
 end
 
 def remove_element_from_end_of_array(array)
-  
+  removed = array.pop()
+  removed
 end
 
 def remove_element_from_start_of_array(array)
-  
+  removed = array.shift()
+  removed
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    first = array[0]
+    first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  last = array[-1]
+  last
 end
